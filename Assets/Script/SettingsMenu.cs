ee8c67cfefd3976b98d4f7e900480ad3aeb51557
generasirabbani <generasirabbani@apps.ipb.ac.id>
2022-08-16 17:53:22 +0700

    initial commit

---
+++ Assets/Script/SettingsMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
