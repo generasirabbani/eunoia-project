98445db98290585915c6066a7113210065435482
generasirabbani <generasirabbani@apps.ipb.ac.id>
2022-08-25 13:08:57 +0700

    added death animation and more

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public int maxHealth = 100;
+    public Animator animator;
     int currentHealth;
+    Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +15,20 @@ public class EnemyScript : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
 
         if(currentHealth <= 0)
         {
@@ -25,6 +38,11 @@ public class EnemyScript : MonoBehaviour
 
     void Die()
     {
-        Debug.Log("Enemy died!");
+        animator.SetBool("isDead", true);
+    }
+
+    public void RemoveEnemy()
+    {
+        Destroy(gameObject);
     }
 }
